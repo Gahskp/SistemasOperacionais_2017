cd8e60c2a0dbb24826f93a52baaf95bf52baeefc
Gahskp <gahskp@gmail.com>
2017-06-24 20:06:51 -0300

    Add files via upload

--- fechadura.c
+++ fechadura.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int pinos = 0, altura, movimentos = 0, pino_ok = 0;
+int pinos = 0, altura, movimentos = 0;
 int *vetor;
 int *vetor_pronto;
-int tamanho_vetor, meio_vetor;
+int tamanho_vetor, divisao;
 
 
 void procura_pronto(int v[]);
@@ -37,29 +37,20 @@ int main()
 
 void procura_pronto(int v[])
 {
-  int i, j, remove = 0, w = 0;
-  meio_vetor = -1;
-  while(w < tamanho_vetor){
-    for(i=0; i<pinos; i++)
-    {
-        //printf("looop2");
-        if(vetor[i] == altura){
-          vetor_pronto[pino_ok] = vetor[i];
-          pino_ok++;
-          for(j=i; j<tamanho_vetor-1; j++)
-          {
-            //printf("loop4");
-            vetor[j] = vetor[j+1];
-          }
-          remove++;
+  int i, j, pronto = 0;
+  for(i=0; i<tamanho_vetor; i++){
+    while (vetor[i] == altura) {
+      if(i == tamanho_vetor-1){
+        vetor[i] = 0;
+      } else{
+        for(j=i; j<tamanho_vetor-1; j++){
+          vetor[j] = vetor[j+1];
         }
-        if(vetor[i] < altura){
-          meio_vetor = i;
-        }
+      }
+      tamanho_vetor--;
     }
-    w++;
   }
-  tamanho_vetor = tamanho_vetor - remove;
+
 }
 
 void ordena(int v[])
@@ -85,42 +76,39 @@ int min_movimentos(int altura, int v[])
 {
   ordena(v);
   procura_pronto(v);
-  while(pino_ok < pinos){
-    int min1, min2, max1, max2;
-    if(vetor[meio_vetor] < altura && vetor[meio_vetor+1] > altura){
-        vetor[meio_vetor]++;
-        vetor[meio_vetor+1]--;
+  printf("\nalo");
+  while(tamanho_vetor > 0){
+    if(vetor[0] < altura && vetor[tamanho_vetor-1] > altura && tamanho_vetor-1 > 0 ){
+      vetor[0]++;
+      vetor[tamanho_vetor]--;
+      movimentos++;
+    } else {
+      if(vetor[0] < altura && vetor[tamanho_vetor-1] < altura && tamanho_vetor-1 > 0){
+        vetor[0]++;
+        vetor[tamanho_vetor-1]++;
         movimentos++;
-    }else {
-        if(meio_vetor >= 0){
-            min1 = meio_vetor;
-          if(meio_vetor - 1 >= 0){
-            min2 = meio_vetor -1;
-            vetor[min1]++;
-            vetor[min2]++;
-          } else{
-            vetor[min1]++;
-          }
+      } else{
+        if(vetor[0] > altura && vetor[tamanho_vetor-1] > altura && tamanho_vetor-1 > 0){
+          vetor[0]--;
+          vetor[tamanho_vetor-1]--;
           movimentos++;
-        } else {
-            //printf("%d %d", meio_vetor, tamanho_vetor);
-            //system("pause");
-            if(meio_vetor + 1 < tamanho_vetor)
-            {
-                max1 = meio_vetor+1;
-                if(meio_vetor+2 < tamanho_vetor){
-                    max2 = meio_vetor+2;
-                    vetor[max1]--;
-                    vetor[max2]--;
-                } else {
-                    vetor[max1]--;
-                }
-                movimentos++;
-                printf("la");
+        } else{
+          if(vetor[0] < altura){
+            vetor[0]++;
+            movimentos++;
+          }else {
+            if(vetor[0] > altura){
+              vetor[0]--;
+              movimentos++;
             }
+          }
         }
+      }
     }
-    procura_pronto(v);
   }
+  /*printf("\nTamanho do vetor: %d ", tamanho_vetor);
+  for(int i = 0; i<tamanho_vetor; i++){
+    printf("\nVetor %d: %d", i, vetor[i] );
+  }*/
   return movimentos;
 }
