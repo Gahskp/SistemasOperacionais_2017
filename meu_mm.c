c8c437ac509a214c27e4b373d549b2f6d2a97596
Gahskp <gahskp@gmail.com>
2017-06-28 19:53:40 -0300

    Add files via upload

--- meu_mm.c
+++ meu_mm.c
@@ -13,25 +13,53 @@ int minha_memoria = 1024;
 void inicializa_mem(int mem);
 void * meu_aloca(int size);
 int merge(struct hole * anterior, struct hole * posterior);
-void meu_libera(void * ponteiro);
+void meu_desaloca(void * ponteiro);
 void mostra_mem();
 
 int main()
 {
+  int *a1, *a2, *a3, *a4;
 	inicializa_mem(minha_memoria);
+  a1 = meu_aloca(35);
+  //printf("%d\n", a1);
+  a2 = meu_aloca(40);
+  //printf("%d\n", a2);
+  a3 = meu_aloca(20);
+  //printf("%d\n", a3);
+  a4 = meu_aloca(30);
+  //printf("%d\n", a4);
+  /*meu_aloca(25);
+  meu_aloca(40);
+  meu_aloca(60);
+  meu_aloca(80);
+  meu_aloca(100);*/
+  meu_desaloca(a1);
+  mostra_mem();
+}
+
+void mostra_mem(){
+  struct hole *atual;
+  atual = header;
+  while (atual != NULL) {
+    if(atual -> tam > 0)
+      printf("Free: %d\n", atual -> tam );
+    else
+      printf("Full: %d\n", atual -> tam );
+    atual = atual -> prox;
+  }
 }
 
 void inicializa_mem(int mem)
 {
 	header = malloc(mem);  //header recebe um ponteiro para o tamanho requesitado
-	header -> tam = mem - sizeof(struct hole);    //O tamanho restante é 1024 menos o tamanho já utilizado por hole 
-	header -> ante = NULL; //Anterior não aponta para nada ainda
-	header -> prox = NULL; //Proximo não aponta para nada ainda
-	//printf("endereço que header recebeu: %d / tamanho que header.tam guarda: %d / tamanho de um hole: %d",
+	header -> tam = mem - sizeof(struct hole);    //O tamanho restante ï¿½ 1024 menos o tamanho jï¿½ utilizado por hole
+	header -> ante = NULL; //Anterior nï¿½o aponta para nada ainda
+	header -> prox = NULL; //Proximo nï¿½o aponta para nada ainda
+	//printf("endereï¿½o que header recebeu: %d / tamanho que header.tam guarda: %d / tamanho de um hole: %d",
 	// header, (header -> tam), sizeof(struct hole));
-	printf("%d \n", header);
-	header = header + header->tam;
-	printf("%d", header);
+	//printf("%d \n", header);
+	//header = header + header->tam;
+	//printf("%d", header);
 }
 
 void *meu_aloca(int size)
@@ -41,30 +69,54 @@ void *meu_aloca(int size)
 	atual = header;
 	while(atual != NULL)
 	{
-		if((atual -> tam) > (size + sizeof(struct hole))){
+    //printf("%d\n", atual->tam );
+		if(atual -> tam > size + sizeof(struct hole))
+    {
 			aux = atual;
 			aux = aux + atual -> tam - size;
-			anterior = atual;
+      atual -> tam -= size + sizeof(struct hole);
+      anterior = atual;
 			atual = aux;
 			atual -> ante = anterior;
 			atual -> prox = anterior -> prox;
 			anterior -> prox = atual;
 			atual -> tam = (-1)*size;
+      //printf("*****%d\n", atual->tam);
 			if(atual -> prox != NULL)
 			{
 				atual -> prox -> ante = atual;
 			}
 			return (aux + sizeof(struct hole));
-		} 
+		}
 		atual = atual -> prox;
 	}
-	return;
 }
 
 
-void meu_libera(void * ponteiro)
+void meu_desaloca(void * ponteiro)
 {
-	struct hole *atual, *anterior, *proximo;
+  struct hole *atual, *anterior, *proximo;
+  atual = ponteiro;
+  atual -> tam *= (-1);
+  anterior = atual -> ante;
+  proximo  = atual -> prox;
+  printf("\n******%d\n", atual->tam);
+  if(anterior != NULL && anterior -> tam > 0){
+    anterior -> prox = proximo;
+    anterior -> tam += atual -> tam + sizeof(struct hole);
+  } else
+  {
+    if(proximo != NULL && proximo -> tam > 0)
+    {
+      proximo -> ante = anterior;
+      proximo -> tam += atual -> tam + sizeof(struct hole);
+    }
+  }
+  proximo  = atual -> prox;
+
+
+
+	/*struct hole *atual, *anterior, *proximo;
 	atual = header;
 	while(atual != NULL)
 	{
@@ -80,8 +132,8 @@ void meu_libera(void * ponteiro)
 			}
 			atual -> tam = atual -> tam *(-1);
 			anterior -> tam = anterior -> tam + atual -> tam + sizeof(struct hole);
-			return;
+			return ;
 		}
 		atual = atual -> prox;
-	}
+	}*/
 }
